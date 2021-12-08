f773fc9aa3fe0502d51ae6f734bc63072a9130f8
Karina (Klamlis) <karina.arias.mt@correo.usa.edu.co>
2021-12-07 21:16:32 -0500

    Class día 1

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+/* import logo from './logo.svg'; */
+import './App.css';
+import InicioSesion from './misComponentes/InicioSesion';
+
+
+function App() {
+  return (
+    <div className = "container">
+      <div className = "row">
+        <div className = "col">
+          <InicioSesion/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
